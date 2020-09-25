7e29f52ea5954ba6ba52a26b27daad348a4d8083
Inlacou <inlacou@gmail.com>
2020-09-25 11:16:00 +0200

    initial

---
+++ mylibrary/src/main/java/com/inlacou/mylibrary/Example.java
@@ -0,0 +1,11 @@
+package com.inlacou.mylibrary;
+
+import android.util.Log;
+
+public class Example {
+
+    public static void test() {
+        Log.d("MY AWESOME LIBRARY", "method from library called");
+    }
+
+}
